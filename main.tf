5493e681d0a57e894982aa6fdbc53bd166591af1
Aishwarya Viratapu <aishwaryaviratapu@gmail.com>
2026-06-18 12:55:21 +0530

    refactor: simplify root directory and add cloud nat

--- main.tf
+++ main.tf
@@ -108,3 +108,205 @@ moved {
   from = google_service_account_iam_member.workload_identity_impersonation
   to   = module.iam.google_service_account_iam_member.workload_identity_impersonation
 }
+
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = var.argocd_namespace
+
+    labels = {
+      "app.kubernetes.io/managed-by" = "terraform"
+      "env"                          = "dev"
+    }
+  }
+
+  depends_on = [module.gke]
+}
+
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "7.4.4"
+  namespace        = kubernetes_namespace.argocd.metadata[0].name
+  create_namespace = false
+
+  set {
+    name  = "controller.replicas"
+    value = "1"
+  }
+
+  set {
+    name  = "server.replicas"
+    value = "1"
+  }
+
+  set {
+    name  = "repoServer.replicas"
+    value = "1"
+  }
+
+  set {
+    name  = "applicationSet.replicas"
+    value = "1"
+  }
+
+  set {
+    name  = "notifications.enabled"
+    value = "false"
+  }
+
+
+  set {
+    name  = "server.extraArgs[0]"
+    value = "--insecure"
+  }
+
+  set {
+    name  = "server.resources.requests.cpu"
+    value = "100m"
+  }
+
+  set {
+    name  = "server.resources.requests.memory"
+    value = "128Mi"
+  }
+
+  set {
+    name  = "server.resources.limits.cpu"
+    value = "500m"
+  }
+
+  set {
+    name  = "server.resources.limits.memory"
+    value = "512Mi"
+  }
+
+  set {
+    name  = "controller.resources.requests.cpu"
+    value = "250m"
+  }
+
+  set {
+    name  = "controller.resources.requests.memory"
+    value = "256Mi"
+  }
+
+  set {
+    name  = "controller.resources.limits.cpu"
+    value = "1000m"
+  }
+
+  set {
+    name  = "controller.resources.limits.memory"
+    value = "1Gi"
+  }
+
+  depends_on = [
+    kubernetes_namespace.argocd,
+    module.gke,
+  ]
+}
+
+resource "kubernetes_manifest" "argocd_root_app" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      name      = "root-apps"
+      namespace = kubernetes_namespace.argocd.metadata[0].name
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL        = "https://github.com/Aishwaryaa12/gke-argocd.git"
+        targetRevision = "HEAD"
+        path           = "gitops/apps"
+      }
+      destination = {
+        server    = "https://kubernetes.default.svc"
+        namespace = kubernetes_namespace.argocd.metadata[0].name
+      }
+      syncPolicy = {
+        automated = {
+          prune    = true
+          selfHeal = true
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    helm_release.argocd
+  ]
+}
+
+resource "google_project_service" "secretmanager" {
+  project            = var.project_id
+  service            = "secretmanager.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_secret_manager_secret" "immich_db_password" {
+  secret_id = "immich-db-password"
+
+  replication {
+    auto {}
+  }
+}
+
+output "cluster_name" {
+  description = "Name of the GKE cluster"
+  value       = module.gke.cluster_name
+}
+
+output "cluster_location" {
+  description = "Zone of the GKE cluster"
+  value       = module.gke.cluster_location
+}
+
+output "cluster_endpoint" {
+  description = "GKE API server endpoint"
+  value       = "https://${module.gke.cluster_endpoint}"
+  sensitive   = true
+}
+
+output "cluster_ca_certificate" {
+  description = "Base64-encoded cluster CA certificate"
+  value       = module.gke.cluster_ca_certificate
+  sensitive   = true
+}
+
+output "kubeconfig_command" {
+  description = "Command to configure kubectl"
+  value       = "gcloud container clusters get-credentials ${module.gke.cluster_name} --zone ${var.zone} --project ${var.project_id}"
+}
+
+output "argocd_namespace" {
+  description = "Namespace where ArgoCD is deployed"
+  value       = kubernetes_namespace.argocd.metadata[0].name
+}
+
+output "argocd_admin_password_command" {
+  description = "Command to retrieve the initial ArgoCD admin password"
+  value       = "kubectl get secret argocd-initial-admin-secret -n ${var.argocd_namespace} -o jsonpath='{.data.password}' | base64 -d && echo"
+}
+
+output "argocd_loadbalancer_ip_command" {
+  description = "Command to retrieve the ArgoCD LoadBalancer external IP"
+  value       = "kubectl get svc argocd-server -n ${var.argocd_namespace} -o jsonpath='{.status.loadBalancer.ingress[0].ip}'"
+}
+
+output "github_actions_service_account_email" {
+  description = "The Service Account email for GitHub Actions to impersonate"
+  value       = module.iam.github_actions_service_account_email
+}
+
+output "workload_identity_provider_name" {
+  description = "The Workload Identity Provider name for GitHub Actions"
+  value       = module.iam.workload_identity_provider_name
+}
+
+output "eso_sa_email" {
+  description = "The External Secrets Operator Service Account email"
+  value       = module.iam.eso_sa_email
+}
--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -81,3 +81,24 @@ resource "google_compute_firewall" "allow_health_checks" {
   source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
   target_tags   = ["gke-node"]
 }
+
+resource "google_compute_router" "router" {
+  project = var.project_id
+  name    = "${var.network_name}-router"
+  network = google_compute_network.vpc.id
+  region  = var.region
+}
+
+resource "google_compute_router_nat" "nat" {
+  project                            = var.project_id
+  name                               = "${var.network_name}-nat"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
